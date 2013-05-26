0daf486186afd2802120aa28dfc9a54122e145f8
takusa <takusagawa@gmail.com>
2013-05-27 04:50:11 +0900

    add heroku setting

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,12 @@
 # -*- encoding: utf-8 -*-
 
 #DB = Sequel.connect('mysql2://root:@localhost/sequel_test')
-options = YAML.load_file("#{File.dirname(__FILE__)}/../config/database.yml")
-DB = Sequel.connect('mysql2:/', options)
+if ENV['CLEARDB_DATABASE_URL']
+  DB = Sequel.connect(ENV['CLEARDB_DATABASE_URL'])
+else
+  options = YAML.load_file("#{File.dirname(__FILE__)}/../config/database.yml")
+  DB = Sequel.connect('mysql2:/', options)
+end
 class Post < Sequel::Model
 
   def self.create_test_data
--- rakefile.rb
+++ rakefile.rb
@@ -7,8 +7,12 @@ namespace :db do
   desc 'migrate database'
   task :migrate do
     #options = {:user=>"root", :password=>"", :database=>"sequel_test"}
-    options = YAML.load_file("#{File.dirname(__FILE__)}/config/database.yml")
-    DB = Sequel.connect("mysql2:/", options)
+    if ENV['CLEARDB_DATABASE_URL']
+      DB = Sequel.connect(ENV['CLEARDB_DATABASE_URL'])
+    else
+      options = YAML.load_file("#{File.dirname(__FILE__)}/config/database.yml")
+      DB = Sequel.connect("mysql2:/", options)
+    end
     Sequel::Migrator.apply(DB, './db/migrate')
   end
 end
